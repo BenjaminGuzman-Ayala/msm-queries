729b71fc4c6dc02ffc6bde1f04ce9bf19baa11a3
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-02-12 17:53:13 +0000

    msm complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,10 +7,23 @@ class DirectorsController < ApplicationController
     the_id = params.fetch("the_id")
 
     matching_records = Director.where({id: the_id})
-
+    pp matching_records
     @the_director = matching_records.at(0)
 
 
     render({template: "/director_template/details"})
+  end
+  def junior
+    @the_name = Director.all.order(dob: :desc).at(0).name
+    @the_dob = Director.all.order(dob: :desc).at(0).dob
+    @the_id = Director.all.order(dob: :desc).at(0).id
+    render({template: "/director_template/junior"})
+    
+  end
+  def senior
+    @the_name = Director.where.not({dob: nil}).all.order(dob: :asc).at(0).name
+    @the_dob = Director.where.not({dob: nil}).all.order(dob: :asc).at(0).dob
+    @the_id = Director.where.not({dob: nil}).all.order(dob: :asc).at(0).id
+    render({template: "/director_template/senior"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {controller: "directors", action: "index"})
+  get("/directors/youngest", {controller: "directors", action: "junior"})
+  get("/directors/eldest", {controller: "directors", action: "senior"})
   get("/directors/:the_id", {controller: "directors", action: "show"})
-  get("/movies", {controller: "movie", action: "index"})
-  get("/movies/:the_id", {controller: "movie", action: "show"})
+  get("/movies", {controller: "movies", action: "index"})
+  get("/movies/:the_id", {controller: "movies", action: "show"})
+  get("/actors", {controller: "actors", action: "index"})
+  get("/actors/:the_id", {controller: "actors", action: "show"})
   
 end
